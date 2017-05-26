dd270c75ed399e5340144b257078d403b00ab273
alen <alen@touzi.org>
2017-05-26 11:58:12 +0800

    fix config_test on use bufio.Scanner

--- config_test.go
+++ config_test.go
@@ -1,14 +1,14 @@
 package config
 
 import (
-  "testing"
-  "strings"
-  "bufio"
-  assert "github.com/pilu/miniassert"
+	"bufio"
+	assert "github.com/pilu/miniassert"
+	"strings"
+	"testing"
 )
 
 func TestParse(t *testing.T) {
-  content := `
+	content := `
   # comment 1
   ; comment 2
 
@@ -43,40 +43,40 @@ func TestParse(t *testing.T) {
 
 
   `
-  reader := bufio.NewReader(strings.NewReader(content))
-  sections, _ := parse(reader, "main")
+	scanner := bufio.NewScanner(strings.NewReader(content))
+	sections, _ := parse(scanner, "main")
 
-  assert.Equal(t, 4, len(sections))
+	assert.Equal(t, 4, len(sections))
 
-  // Main section
-  main_section := sections["main"]
-  assert.Equal(t, 2, len(main_section))
-  assert.Equal(t, "1", main_section["foo"])
-  assert.Equal(t, "2", main_section["bar"])
+	// Main section
+	main_section := sections["main"]
+	assert.Equal(t, 2, len(main_section))
+	assert.Equal(t, "1", main_section["foo"])
+	assert.Equal(t, "2", main_section["bar"])
 
-  // Section 1
-  section_1 := sections["section_1"]
-  assert.Equal(t, 5, len(section_1))
-  assert.Equal(t, "3", section_1["foo"])
-  assert.Equal(t, "4", section_1["bar"])
-  assert.Equal(t, "5 6", section_1["baz"])
-  assert.Equal(t, "7", section_1["qux"])
-  assert.Equal(t, "", section_1["quux"])
+	// Section 1
+	section_1 := sections["section_1"]
+	assert.Equal(t, 5, len(section_1))
+	assert.Equal(t, "3", section_1["foo"])
+	assert.Equal(t, "4", section_1["bar"])
+	assert.Equal(t, "5 6", section_1["baz"])
+	assert.Equal(t, "7", section_1["qux"])
+	assert.Equal(t, "", section_1["quux"])
 
-  // Section 2
-  section_2 := sections["section_2"]
-  assert.Equal(t, 9, len(section_2))
-  assert.Equal(t, "1", section_2["a"])
-  assert.Equal(t, "2", section_2["b"])
-  assert.Equal(t, "3", section_2["c"])
-  assert.Equal(t, "4", section_2["d"])
-  assert.Equal(t, "5", section_2["e"])
-  assert.Equal(t, "6", section_2["f"])
-  assert.Equal(t, "7", section_2["g"])
-  assert.Equal(t, "8", section_2["h"])
-  assert.Equal(t, "http://example.com", section_2["url"])
+	// Section 2
+	section_2 := sections["section_2"]
+	assert.Equal(t, 9, len(section_2))
+	assert.Equal(t, "1", section_2["a"])
+	assert.Equal(t, "2", section_2["b"])
+	assert.Equal(t, "3", section_2["c"])
+	assert.Equal(t, "4", section_2["d"])
+	assert.Equal(t, "5", section_2["e"])
+	assert.Equal(t, "6", section_2["f"])
+	assert.Equal(t, "7", section_2["g"])
+	assert.Equal(t, "8", section_2["h"])
+	assert.Equal(t, "http://example.com", section_2["url"])
 
-  // Section 3
-  section_3 := sections["section_3"]
-  assert.Equal(t, 0, len(section_3))
+	// Section 3
+	section_3 := sections["section_3"]
+	assert.Equal(t, 0, len(section_3))
 }
